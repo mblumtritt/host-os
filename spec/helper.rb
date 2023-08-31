fe85c438c6cdbbe5784701443794ff9fb9736ed6
Mike Blumtritt <mike.blumtritt@pm.me>
2023-08-31 08:41:49 +0200

    Use documentation style for tests

--- spec/helper.rb
+++ spec/helper.rb
@@ -5,3 +5,4 @@ require_relative '../lib/host-os/support'
 
 $stdout.sync = $stderr.sync = $VERBOSE = true
 RSpec.configure(&:disable_monkey_patching!)
+RSpec.configure { |config| config.formatter = :documentation }
