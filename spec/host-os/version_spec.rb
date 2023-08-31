e5e72c5b4e4d97cecdc40559f173c053229f990d
Mike Blumtritt <mike.blumtritt@pm.me>
2023-08-31 08:41:31 +0200

    Tidy-up version test

--- spec/host-os/version_spec.rb
+++ spec/host-os/version_spec.rb
@@ -3,7 +3,8 @@
 require_relative '../helper'
 require_relative '../../lib/host-os/version'
 
-RSpec.describe HostOS::VERSION do
+RSpec.describe 'HostOS::VERSION' do
+  subject(:version) { HostOS::VERSION }
   it { is_expected.to match(/\A[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\z/) }
   it { is_expected.to be_frozen }
 end
